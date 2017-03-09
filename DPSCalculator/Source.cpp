3ac8de4ede05f4d8db338ad84dd4c7ff6b22c15f
Squishofdoom <spamisbadjerk@live.com>
2017-03-09 15:19:19 -0700

    Revert "Updated Calculation"
    This reverts commit a02b3bbc86a9a9f271b074322dbd5b95b70a4dec.

--- DPSCalculator/Source.cpp
+++ DPSCalculator/Source.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <cmath>
 
 using namespace std;
 
@@ -16,7 +15,6 @@ struct wep {
 	int maxRange;
 	int minRange;
 	int magSize;
-	int velocity;
 	double headShotMult;
 	int RPM;
 	string damageType;
@@ -91,54 +89,20 @@ void DPSCalc() {
 	}//end of while loop
 
 	double dps;//damage per second
-	double idealDPS;//dps assuming 100% accuracy and infinite ammo
+	double idealDPS;
 	double rps;//rounds per second
-	double damage;//damage after linear interpolation
-	double distance;//distance from target in meters
-	double BTK;//bullets to kill
-	double basicTTK;//time to kill, 100% accuracy on the body
-	double simpleTTK;//time to kill, user-provided accuracy on the body
-	double accuracy;
-	int missedShots;
-	
-	
+	double damage;
+	double distance;
+
 	cout << "What distance are you shooting from?" << endl;
 
 	cin >> distance;
 
-	cout << "What is your accuracy percentage(do not include the % sign, just the number)?" << endl;
-
-	cin >> input;
-
-	accuracy = (double)input / 100.0;
-	accuracy = 1.0 - accuracy;
-
 	damage = weapon.maxDamage - (distance - weapon.minRange) / (weapon.maxRange - weapon.minRange) * (weapon.maxDamage - weapon.minDamage);
-
-	rps = weapon.RPM / 60.0;
-
-	idealDPS = rps * damage;
-
-	BTK = 1000 / damage;
-	BTK = ceil(BTK);
-
-	missedShots = BTK * accuracy;
-	simpleTTK = ((BTK + missedShots) - 1) * 60 / weapon.RPM;
-
-	basicTTK = (BTK - 1) * 60 / weapon.RPM;
-
-	
+	rps = weapon.RPM / 60;
 
 
-
-	cout << rps << " rounds per second." << endl;
 	cout << damage << " damage at " << distance << " meters." << endl;
-	cout << idealDPS << " ideal DPS(100% accuracy, infinite ammo)" << endl;
-	cout << BTK << " bullets to kill." << endl;
-	cout << basicTTK << " seconds to kill, assuming 100% accuracy on the body." << endl;
-	cout << simpleTTK << " seconds to kill, assuming " << input << "% accuracy on the body." << endl;
-
-	
 
 
 }//end of function DPSCalc
@@ -173,10 +137,6 @@ wep manualInput(wep weapon) {
 	cin >> weapon.RPM;
 	cout << endl;
 
-	cout << "Bullet velocity: ";
-	cin >> weapon.velocity;
-	cout << endl;
-
 	cout << "Headshot multiplier(default is 2.0): ";
 	cin >> weapon.headShotMult;
 	cout << endl;
@@ -187,14 +147,13 @@ wep manualInput(wep weapon) {
 
 	cout << "Would you like to save this weapon into file? 1 for yes, 2 for no." << endl;
 	cin >> input;
-	cin.ignore();
 
 	weapon.damageType = convertLower(weapon.damageType);
 
 	if (input == 1) {
 
 		cout << "Name of weapon: ";
-		getline(cin, weapon.name);
+		cin >> weapon.name;
 		cout << endl;
 
 		weapon.name = convertLower(weapon.name);
@@ -236,10 +195,6 @@ wep manualInput() {
 	cin >> weapon.RPM;
 	cout << endl;
 
-	cout << "Bullet velocity: ";
-	cin >> weapon.velocity;
-	cout << endl;
-
 	cout << "Headshot multiplier(default is 2.0): ";
 	cin >> weapon.headShotMult;
 	cout << endl;
@@ -247,10 +202,9 @@ wep manualInput() {
 	cout << "Damage type(\"infantry\" for infantry weapons): ";
 	cin >> weapon.damageType;
 	cout << endl;
-	cin.ignore();
 
 	cout << "Name of weapon: ";
-	getline(cin, weapon.name);
+	cin >> weapon.name;
 	cout << endl;
 
 	weapon.damageType = convertLower(weapon.damageType);
@@ -288,7 +242,6 @@ wep import(wep weapon) {
 			weapons >> weapon.maxRange;
 			weapons >> weapon.magSize;
 			weapons >> weapon.RPM;
-			weapons >> weapon.velocity;
 			weapons >> weapon.headShotMult;
 			weapons >> weapon.damageType;
 
@@ -319,7 +272,6 @@ void save(wep weapon) {
 	weapons << weapon.maxRange << endl;
 	weapons << weapon.magSize << endl;
 	weapons << weapon.RPM << endl;
-	weapons << weapon.velocity << endl;
 	weapons << weapon.headShotMult << endl;
 	weapons << weapon.damageType << endl;
 	weapons << "<END>" << endl;
